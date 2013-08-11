58bc80c80af27b0fad63a759565bdab158d08aed
BilboAtBagEnd <bilboatbagend@mail.lairoflith.com>
2013-08-10 21:29:28 -0700

    Use port number.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ AppLairoflithCom::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'app.lairoflith.com' }
+  config.action_mailer.default_url_options = { :host => 'app.lairoflith.com:8300' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
