e55b9dba560a3c6cc9ec881047d3c9f452821da0
BilboAtBagEnd <bilboatbagend@mail.lairoflith.com>
2013-08-17 10:37:58 -0700

    Correct listing of users.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 
   def index
     #@characters = Character.connection.select_all('SELECT characters.*, users.slug as user_slug, users.name as user_name from characters, users where characters.user_id = users.id order by created_at desc limit 50')
-    @characters = Character.joins(:user).order(:created_at).limit(50)
+    @characters = Character.joins(:user).order('created_at desc').limit(10)
   end
 
   def bbcode
