459eaf018ef24dffd3f9a27735576b69a6953055
BilboAtBagEnd <bilboatbagend@mail.lairoflith.com>
2013-08-17 08:57:00 -0700

    Use joins() instead of manual query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,8 @@ class HomeController < ApplicationController
   protect_from_forgery :except => [:bbcode]
 
   def index
-    @characters = Character.connection.select_all('SELECT characters.*, users.slug as user_slug, users.name as user_name from characters, users where characters.user_id = users.id order by created_at desc limit 50')
+    #@characters = Character.connection.select_all('SELECT characters.*, users.slug as user_slug, users.name as user_name from characters, users where characters.user_id = users.id order by created_at desc limit 50')
+    @characters = Character.joins(:user).order(:created_at).limit(50)
   end
 
   def bbcode
