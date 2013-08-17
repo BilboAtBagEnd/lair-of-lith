da2e5df91f7814a1b0f30beff417222ea0c0007f
BilboAtBagEnd <bilboatbagend@mail.lairoflith.com>
2013-08-17 13:16:03 -0700

    Added gravatars.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def self.parseBBCode(bbcode)
     RbbCode.new.convert(Sanitize.clean bbcode.to_s)
   end
+
+  def self.gravatar(user, size = 64)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size.to_i}"
+  end
 end
