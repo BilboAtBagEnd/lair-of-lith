1aa9c011f27f657c22af2c9ad82ccab08db7a02a
BilboAtBagEnd <bilboatbagend@mail.lairoflith.com>
2013-08-18 10:01:40 -0700

    Rollback schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130817172138) do
+ActiveRecord::Schema.define(version: 20130818101814) do
 
   create_table "character_versions", force: true do |t|
     t.integer  "character_id"
