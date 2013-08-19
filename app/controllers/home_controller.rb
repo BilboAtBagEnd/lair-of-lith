39355ff537a47720a0525f7ef00c0ee538b1aace
BilboAtBagEnd <bilboatbagend@mail.lairoflith.com>
2013-08-19 09:26:33 -0700

    Better index queries.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,8 @@ class HomeController < ApplicationController
 
   def index
     #@characters = Character.connection.select_all('SELECT characters.*, users.slug as user_slug, users.name as user_name from characters, users where characters.user_id = users.id order by created_at desc limit 50')
-    @characters = Character.joins(:user).order('created_at desc').limit(10)
+    @characters = Character.includes(:user).order('characters.created_at desc').limit(10).references(:user)
+    @character_total = Character.all.count
   end
 
   def bbcode
