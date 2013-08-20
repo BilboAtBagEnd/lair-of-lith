eae52f4f4782006f7b24a0426123c281cabc293d
BilboAtBagEnd <bilboatbagend@mail.lairoflith.com>
2013-08-20 00:02:21 -0700

    more tinkering with tags

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
   protect_from_forgery :except => [:bbcode]
 
   def index
-    #@characters = Character.connection.select_all('SELECT characters.*, users.slug as user_slug, users.name as user_name from characters, users where characters.user_id = users.id order by created_at desc limit 50')
     @characters = Character.includes(:user).order('characters.created_at desc').limit(10).references(:user)
     @character_total = Character.all.count
+    @popular_tags = Character.tag_counts.order('count desc, name asc').limit(10)
   end
 
   def bbcode
