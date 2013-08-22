980b455751a9b7ba2b882985dbfaa2a2ed5a7316
BilboAtBagEnd <bilboatbagend@mail.lairoflith.com>
2013-08-21 18:49:21 -0700

    Present characters ready for comment on homepage.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,8 @@ class HomeController < ApplicationController
   def index
     @characters = Character.includes(:user).order('characters.created_at desc').limit(10).references(:user)
     @character_total = Character.all.count
-    @popular_tags = Character.tag_counts.order('count desc, name asc').limit(10)
+    @popular_tags = Character.tag_counts.order('count desc, name asc').limit(20)
+    @characters_for_comment = Character.find_by_status('REVIEW').order('characters.updated_at desc').limit(10).includes(:user).references(:user)
   end
 
   def bbcode
