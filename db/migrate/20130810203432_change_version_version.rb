632e547db01f878ff177dc66c86c1cf4fade2ee3
A. Jarvis <arachne.jar@gmail.com>
2014-10-20 05:45:24 +0000

    Schema changes

--- db/migrate/20130810203432_change_version_version.rb
+++ db/migrate/20130810203432_change_version_version.rb
@@ -1,5 +1,5 @@
 class ChangeVersionVersion < ActiveRecord::Migration
   def change
-    change_column(:character_versions, :version, :integer)
+    change_column(:character_versions, :version, 'integer USING CAST(version AS integer)')
   end
 end
